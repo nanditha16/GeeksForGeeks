2ff66076ba6da68bb8e872db904cd4395d41b719
nanditha16 <nanditha.murthy@gmail.com>
2020-04-05 12:51:40 -0400

    Test example

---
+++ geeksForGeeks/src/main/java/com/test/citibank/Test.java
@@ -0,0 +1,26 @@
+package com.test.citibank;
+
+import java.util.stream.IntStream;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String inverted = "null";
+//				
+//		String reversed = inverted.chars()
+//			    .collect(StringBuilder::new, (b,c) -> b.insert(0,(char)c), (b1,b2) -> b1.insert(0, b2))
+//			    .toString();
+//		
+//		System.out.println(reversed);
+		int j = 0;
+	
+	
+
+
+	}
+	
+	
+}
+
+
